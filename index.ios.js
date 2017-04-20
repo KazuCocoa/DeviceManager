ac26c3b2c05749e25352196fe1d82f0a05e9579a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-04-20 09:15:06 +0900

    remove example and add some components

--- index.ios.js
+++ index.ios.js
@@ -4,14 +4,47 @@
  * @flow
  */
 
+/**
+ * http://shoutem.github.io/docs/ui-toolkit/components/
+ */
+
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import { Examples } from '@shoutem/ui';
+import {
+  AppRegistry,
+  StyleSheet,
+ } from 'react-native';
+import {
+  Text,
+  Button,
+  View,
+  ListView,
+  Tile,
+  Title,
+  Subtitle,
+  Overlay,
+  Screen,
+  Heading,
+  NavigationBar
+ } from '@shoutem/ui';
 
 export default class DeviceManager extends Component {
   render() {
     return (
-      <Examples />
+      <Screen>
+        <NavigationBar
+          centerComponent={<Title>TITLE</Title>}
+        />
+
+        <View styleName="horizontal flexible">
+          <Button styleName="confirmation">
+            <Text>REMOVE</Text>
+          </Button>
+
+          <Button styleName="confirmation dark">
+            <Text>UPDATE</Text>
+          </Button>
+        </View>
+      </Screen>
     );
   }
 }
